dfb0a7f7b80a1cfef58f78e5d4cd08ef40d6d08e
orlando <orlandolana09@gmail.com>
2020-11-13 12:22:50 +0700

    fix indent

--- TimeComplexBenchmark/main.go
+++ TimeComplexBenchmark/main.go
@@ -4,26 +4,27 @@ import "fmt"
 
 // Value struct
 type Value struct {
-    num int
+	num int
 }
+
 // AddUpTo func1 O(N)
 func (v Value) AddUpTo() int {
-    var total int = 0
-	
+	var total int = 0
+
 	for i := 1; i <= v.num; i++ {
-		total +=i
+		total += i
 	}
 	return total
 }
+
 // AddUpToOpt func2 Big O(1)
 func (v Value) AddUpToOpt() int {
-    return v.num*(v.num+1)/2
-}
-func main(){
-	 var  Val Value
-	 Val.num = 9223372010
-	 fmt.Println(Val.AddUpTo())
-	 fmt.Println(Val.AddUpToOpt())
+	return v.num * (v.num + 1) / 2
 }
 
-
+func main() {
+	var Val Value
+	Val.num = 9223372010
+	fmt.Println(Val.AddUpTo())
+	fmt.Println(Val.AddUpToOpt())
+}
